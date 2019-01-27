f0f4850e5c89ecc5d89cd967624d54a668ae0aa7
jeffchoi <ihello0720@gmail.com>
2019-01-27 20:27:31 +0900

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ const { getUsers, getUser } = require('../controller');
 const router = new Router();
 
 router.get('/', (ctx) => ctx.body = '안녕하세요, 최영훈입니다.');
+router.get('/deploy', (ctx) => ctx.body = '무중단배포 성공');
 
 router.get('/users', getUsers);
 router.get('/users/:id', getUser);
